956883d21ed796b9d48408cbcf5874a6d9f27185
sting <wholesomegodlist@gmail.com>
2020-06-11 03:07:22 -0500

    extra layer of security on posts

--- process_post.py
+++ process_post.py
@@ -3,7 +3,23 @@ import json
 from praw.models import Submission
 
 import process_comment
+import sqlite3
+from sqlite3 import Error
 
+
+def create_connection(path):
+	connection = None
+	try:
+		connection = sqlite3.connect(path)
+		print("Connected to the posts database")
+	except Error as e:
+		print(f"The error '{e}' occurred")
+
+	return connection
+
+
+conn = create_connection('posts.db')
+c = conn.cursor()
 
 config = json.load(open('config.json'))
 
@@ -14,9 +30,14 @@ async def process_post(submission: Submission):
 	# Do not respond to memes!
 	# Or non-image posts.
 	# god I miss nullish coalescing
-	if (hasattr(submission, 'link_flair_text') and submission.link_flair_text and submission.link_flair_text.lower() == 'meme') or submission.is_self:
+	if (hasattr(submission, 'link_flair_text') and submission.link_flair_text and 'meme' in submission.link_flair_text.lower()) or submission.is_self:
 		print("Either this is flaired Meme or this is a self-post.")
 		return
+
+	# Never ask for sauce twice on the same post.
+	c.execute('SELECT * FROM allposts WHERE id=?', (submission.id,))
+	if c.fetchone():
+		print("Duplicate post. Not sure how this happened, but it did.")
 
 	print("This is an actual post, asking for sauce...")
 	await ask_for_sauce(submission)
@@ -32,3 +53,4 @@ async def ask_for_sauce(submission: Submission):
 
 	comment.mod.distinguish(how='yes', sticky=True)
 	process_comment.register_pending_sauce(submission.author.name, submission.id, comment.id)
+	c.execute('INSERT INTO allposts VALUES (?)', (submission.id,))
