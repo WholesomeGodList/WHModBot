429df2ed8c3cfa7618a14aa0764d65b5eaead2a4
sting <wholesomegodlist@gmail.com>
2020-11-13 10:13:56 -0600

    remove file

--- testing2.py
+++
@@ -1,7 +0,0 @@
-def make_chocolate(small, big, goal):
-	print((goal - min(big, goal // 5) * 5))
-	return (-1, (goal - min(big, goal // 5) * 5))[(big >= goal // 5 >= 0) and ((goal - min(big, goal // 5) * 5) <= small)]
-
-
-print(make_chocolate(6, 1, 11))
-
