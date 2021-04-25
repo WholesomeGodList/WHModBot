f75989a581e9e5dea794515a502e2cc61396a54d
TheCommanderFort <thecommanderfort@gmail.com>
2021-04-25 17:18:21 -0300

    Change Warning to Note

--- process_comment.py
+++ process_comment.py
@@ -87,7 +87,7 @@ async def process_comment(comment: Comment, reddit: Reddit):
 					print(entry)
 					god_list = f"\\-\\-\\-\n\n[Wholesome Hentai God List #{entry['id']}](https://wholesomelist.com/list/{entry['uuid']})  \n" \
 					           '\n' + (# f'**Tier: {entry["tier"]}**\n\n' + (
-						           '' if (entry['warning'] == 'None') else f'**Warning:** {entry["warning"]}  \n') + \
+						           '' if (entry['warning'] == 'None') else f'**Note:** {entry["warning"]}  \n') + \
 					           f'**Tags:** ' + ('None' if len(entry["tags"]) == 0 else entry['tags']) + "\n\n"
 			except Exception:
 				god_list = ""
@@ -395,7 +395,7 @@ async def process_comment(comment: Comment, reddit: Reddit):
 						print(entry)
 						god_list = f"\\-\\-\\-\n\n[Wholesome Hentai God List #{entry['id']}](https://wholesomelist.com/list/{entry['uuid']})  \n" \
 						           '\n' + ( # f'**Tier: {entry["tier"]}**\n\n' + (
-							           '' if (entry['warning'] == 'None') else f'**Warning:** {entry["warning"]}  \n') + \
+							           '' if (entry['warning'] == 'None') else f'**Note:** {entry["warning"]}  \n') + \
 						           f'**Tags:** ' + ('None' if len(entry["tags"]) == 0 else entry['tags']) + "\n\n"
 				except Exception:
 					god_list = ""
