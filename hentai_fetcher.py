9b2410844c192906af508612457f7ed4ddfe1e64
TheCommanderFort <thecommanderfort@gmail.com>
2024-08-13 18:22:14 -0300

    Rewrite hentai_fetcher logic
    * Compile global regexes instead of generating them per call
    * Improve license comparison, adding some extra checks to avoid unexpected errors
    * Use nhentai API for the data, avoiding compiling lots of regexes
    * Ditch pyfunctional for a simple match case
    * Return namespaces for E-Hentai fetching

--- hentai_fetcher.py
+++ hentai_fetcher.py
@@ -3,14 +3,50 @@ import re
 import traceback
 
 import aiohttp
-from bs4 import BeautifulSoup
 import asyncio
-from functional import seq
+from bs4 import BeautifulSoup
+
+
+LICENSED_MAGAZINES = {
+	"always_licensed": [
+		"happining",
+		"aoha",
+		"weekly kairakuten",
+		"dascomi",
+	],
+	"partially_licensed": {
+		"kairakuten": ["2015-06", "9999-99", None, None],
+		"x-eros": [None, None, 30, -1],
+		"shitsurakuten": ["2016-04", "9999-99", None, None],
+		"kairakuten beast": ["2016-12", "9999-99", None, None],
+		"bavel": ["2017-06", "9999-99", None, None],
+		"europa": ["2017-04", "9999-99", 11, -1],
+		"girls form": [None, None, 13, 16],
+		"koh": ["2013-12", "2014-07", 1, 2]
+	}
+}
+
+# nhentai regexes
+NUMBERS_REGEX = re.compile(r"/g/(\d{1,6})/?")
+
+# magazine date regexes
+DATE_PATTERN = re.compile(r"(\d+)-(\d+)")
+DATE_PATTERN_2 = re.compile(r"\D*(\d+)\D*")
+
+# Generic regexes
+TITLE_EXTRACTOR = re.compile(
+	r"^(?:\s*(?:=.*?=|<.*?>|\[.*?]|\(.*?\)|{.*?})\s*)*(?:[^\[|\](){}<>=]*\s*\|\s*)?([^\[|\](){}<>=]*?)(?:\s*(?:=.*?=|<.*?>|\[.*?]|\(.*?\)|{.*?})\s*)*$")
+
+# special magazine regexes
+# See if this doujin has a magazine associated with it
+MAGAZINE_REGEX = re.compile(r".*\(\s*comic\s*(.+?)\s*(?:vol\.)?\s*((\d|-|#|,|\s)*)\)")
+# (girls forM is annoying because it doesn't have a COMIC, so I have to use another regex)
+GIRLS_FORM_REGEX = re.compile(r".*\(\s*girls\s*form\s*(?:vol\.)?\s*(.+?)\)")
+# (so is Weekly Kairakuten)
+WEEKLY_MAG_REGEX = re.compile(r".*\(\s*weekly\s*kairakuten\s*(?:vol\.)?\s*(.+)\)")
 
 
 def date_num_compare(magazine: str, issue: str) -> bool:
-	global licensed_magazines
-
 	if " " in issue:
 		issues = issue.split(" ")
 
@@ -21,27 +57,27 @@ def date_num_compare(magazine: str, issue: str) -> bool:
 			ans = date_num_compare(magazine, issues[1])
 			return ans
 
-	pattern = re.compile(r"(\d+)-(\d+)")
-	pattern2 = re.compile(r"\D*(\d+)\D*")
-
-	match = re.match(pattern, issue)
-	match2 = re.match(pattern2, issue)
+	match = DATE_PATTERN.match(issue)
+	match2 = DATE_PATTERN_2.match(issue)
 
 	if match is not None:
-		startdate = licensed_magazines[magazine][1]
-		enddate = licensed_magazines[magazine][2]
+		startdate = LICENSED_MAGAZINES["partially_licensed"][magazine][0]
+		enddate = LICENSED_MAGAZINES["partially_licensed"][magazine][1]
 
-		startmatch = re.match(pattern, startdate)
-		endmatch = re.match(pattern, enddate)
+		startmatch =  DATE_PATTERN.match(startdate)
+		endmatch = DATE_PATTERN.match(enddate)
 
 		issueyear = int(match.group(1))
 		issuemonth = int(match.group(2))
 
-		startyear = int(startmatch.group(1))
-		startmonth = int(startmatch.group(2))
+		if startmatch and endmatch:
+			startyear = int(startmatch.group(1))
+			startmonth = int(startmatch.group(2))
 
-		endyear = int(endmatch.group(1))
-		endmonth = int(endmatch.group(2))
+			endyear = int(endmatch.group(1))
+			endmonth = int(endmatch.group(2))
+		else:
+			raise AttributeError('Failed to find issue number!')
 
 		if startyear > issueyear or issueyear > endyear:
 			return False
@@ -49,10 +85,11 @@ def date_num_compare(magazine: str, issue: str) -> bool:
 			return False
 		elif endyear == issueyear and endmonth < issuemonth:
 			return False
+
 		return True
-	else:
-		startnum = licensed_magazines[magazine][3]
-		endnum = licensed_magazines[magazine][4]
+	elif match2 is not None:
+		startnum = LICENSED_MAGAZINES["partially_licensed"][magazine][2]
+		endnum = LICENSED_MAGAZINES["partially_licensed"][magazine][3]
 
 		if endnum == -1:
 			endnum = 100000000
@@ -60,26 +97,8 @@ def date_num_compare(magazine: str, issue: str) -> bool:
 		issuenum = int(match2.group(1))
 
 		return startnum <= issuenum <= endnum
-
-
-def always_licensed(magazine, issue):
-	return True
-
-
-licensed_magazines = {
-	"kairakuten": [date_num_compare, "2015-06", "9999-99", None, None],
-	"x-eros": [date_num_compare, None, None, 30, -1],
-	"shitsurakuten": [date_num_compare, "2016-04", "9999-99", None, None],
-	"kairakuten beast": [date_num_compare, "2016-12", "9999-99", None, None],
-	"bavel": [date_num_compare, "2017-06", "9999-99", None, None],
-	"europa": [date_num_compare, "2017-04", "9999-99", 11, -1],
-	"girls form": [date_num_compare, None, None, 13, 16],
-	"happining": [always_licensed],
-	"aoha": [always_licensed],
-	"weekly kairakuten": [always_licensed],
-	"dascomi": [always_licensed],
-	"koh": [date_num_compare, "2013-12", "2014-07", 1, 2]
-}
+	else:
+		return False
 
 
 # merge fetching everything lol
@@ -94,55 +113,58 @@ async def process_site(link: str) -> tuple[str, list[str] | None, str | None, li
 		language = list()
 
 		if 'nhentai' in link:
-			response = await session.get(link)
+			numbers_match = NUMBERS_REGEX.search(link)
+			numbers = int(numbers_match.group(1))
+
+			api_url = f"https://nhentai.net/api/gallery/{numbers}"
+
+			response = await session.get(api_url)
 			code = response.status
-			if code == 503 or code == 403:  # cloudflare IUAM
+
+			if code == 403 or code == 503 or 'application/json' not in response.headers.get('Content-Type', ''):  # cloudflare IUAM
 				return "Cloudflare IUAM", None, None, None, None, None, None
 
-			page = await response.text()
-			soup = BeautifulSoup(page, 'html.parser')
-			title = soup.find_all('h1', class_="title")[0].get_text()
+			data = await response.json()
 
-			tag_extractor = re.compile(r"/tag/(.*)/")
-			artist_extractor = re.compile(r"/artist/(.*)/")
-			parody_extractor = re.compile(r"/parody/(.*)/")
-			character_extractor = re.compile(r"/character/(.*)/")
-			page_extractor = re.compile(r"/search/\?q=pages.*")
-			language_extractor = re.compile(r"/language/(.*)/")
-			link_pile = soup.find_all('a', href=tag_extractor)
-			artist_pile = soup.find_all('a', href=artist_extractor)
-			parody_pile = soup.find_all('a', href=parody_extractor)
-			character_pile = soup.find_all('a', href=character_extractor)
-			language_pile = soup.find_all('a', href=language_extractor)
-			pages = int(soup.find('a', class_='tag', href=page_extractor).find('span', class_='name').string.strip())
+			title = data["titles"]["english"] if data["titles"]["english"] else data["titles"]["pretty"] if data["titles"]["pretty"] else "None"
 
-			for cur_link in link_pile:
-				link_text = cur_link.find('span', class_='name')
-				tags.append(link_text.text)
+			for tag in data["tags"]:
+				match tag["type"]:
+					case 'language':
+						if tag["name"] != 'translated' and tag["name"] != 'rewrite':
+							language.append(tag["name"])
+					case 'parody':
+						if tag["name"] != 'original':
+							parody_match = re.search(fr'(?i)({tag["name"]})', data["titles"]["english"])
 
-			for cur_link in artist_pile:
-				link_text = cur_link.find('span', class_='name')
-				temp = link_text.text.split(' ')
-				for i in range(len(temp)):
-					temp[i] = temp[i].capitalize()
-				artists.append(' '.join(temp))
+							if parody_match:
+								parodies.append(parody_match.group(1))
+							else:
+								parodies.append(tag["name"].title())
+					case 'character':
+						characters.append(tag["name"])
+					case 'tag':
+						tags.append(tag["name"])
+					case 'artist':
+						artist_match = re.search(fr'(?i)({tag["name"]})', data["titles"]["english"])
 
-			for cur_link in parody_pile:
-				link_text = cur_link.find('span', class_='name')
-				if link_text.text != 'original':
-					parodies.append(link_text.text)
+						if artist_match:
+							artists.append(artist_match.group(1))
+						else:
+							artists.append(tag["name"].title())
+					case _:
+						continue
 
-			for cur_link in character_pile:
-				link_text = cur_link.find('span', class_='name')
-				characters.append(link_text.text)
-
-			for cur_link in language_pile:
-				link_text = cur_link.find('span', class_='name')
-				language.append(link_text.text)
-
-		elif 'e-hentai' in link:
+			pages = data["num_pages"]
+		else:
 			try:
-				gallery_id, gallery_token = re.search(r'/g/(\d+?)/(.+?)/', link).group(1, 2)
+				match = re.search(r'/g/(\d+?)/(.+?)/', link)
+
+				if match is None:
+					raise AttributeError('Failed to find gallery ID and token')
+
+				gallery_id, gallery_token = match.group(1, 2)
+
 				response = await session.post('https://api.e-hentai.org/api.php',
 					json={
 						"method": "gdata",
@@ -151,38 +173,56 @@ async def process_site(link: str) -> tuple[str, list[str] | None, str | None, li
 						],
 						"namespace": 1
 					})
+
 				resp = await response.json(content_type='text/html')
+
 				if 'error' in resp['gmetadata'][0]:
 					raise RuntimeError(resp['error'])
+
 				data = resp['gmetadata'][0]
 
 				title = data['title'].strip()
-				tags = list((seq(data['tags'])
-						.filter(lambda s: re.match(r'(?:female|male|mixed|other):', s))))
-				artists = list((seq(data['tags'])
-							.filter(lambda s: re.match(r'artist', s))
-							.map(lambda s: re.match(r'artist:(.+)', s)[1])
-							.map(lambda s: ' '.join((w.capitalize() for w in s.split())))))
-				parodies = list((seq(data['tags'])
-							.filter(lambda s: re.match(r'parody', s))
-							.map(lambda s: re.match(r'parody:(.+)', s)[1])
-							.filter(lambda s: s != 'original')))
-				characters = list((seq(data['tags'])
-							.filter(lambda s: re.match(r'character', s))
-							.map(lambda s: re.match(r'character:(.+)', s)[1])))
+
+				for tag in data['tags']:
+					if ':' in tag:
+						namespace, name = tag.split(':')
+
+						match namespace:
+							case 'artist':
+								artist_match = re.search(fr'(?i)({name})', title)
+
+								if artist_match:
+									artists.append(artist_match.group(1))
+								else:
+									artists.append(name.title())
+							case 'parody':
+								if name != 'original':
+									parody_match = re.search(fr'(?i)({name})', data["title"])
+
+									if parody_match:
+										parodies.append(parody_match.group(1))
+									else:
+										parodies.append(name.title())
+							case "character":
+								characters.append(name)
+							case "language":
+								if name not in ["translated", "rewrite"]:
+									language.append(name)
+							case 'female' | "male" | "mixed" | "other":
+								tags.append(tag)
+							case _:
+								continue
+				
 				pages = int(data['filecount'])
-				language = list((seq(data['tags'])
-							.filter(lambda s: re.match(r'language', s))
-							.map(lambda s: re.match(r'language:(.+)', s)[1])))
-			except RuntimeError as e:
-				print('E-hentai error: ' + str(e))
+			except (AttributeError, RuntimeError) as e:
+				print(f'E-hentai error: {e}')
 				return 'E-Hentai error', None, None, None, None, None, None
 
 		print([title, tags, ', '.join(artists), parodies, characters, pages, language])
 		return title, tags, ', '.join(artists), parodies, characters, pages, language
 
 
-async def check_link(link):
+async def check_link(link: str) -> tuple[str | None, bool | None, str | list[list[str] | str | int | None]]:
 	title, tags, artists, parodies, characters, pages, lang = await process_site(link)
 
 	if title == "Cloudflare IUAM":
@@ -190,30 +230,24 @@ async def check_link(link):
 	elif title == 'E-hentai error':
 		return None, None, "E-hentai error"
 
-	pattern_extractor = re.compile(
-		r"^(?:\s*(?:=.*?=|<.*?>|\[.*?]|\(.*?\)|{.*?})\s*)*(?:[^[|\](){}<>=]*\s*\|\s*)?([^\[|\](){}<>=]*?)(?:\s*(?:=.*?=|<.*?>|\[.*?]|\(.*?\)|{.*?})\s*)*$")
-	match = pattern_extractor.match(title)
+	match = TITLE_EXTRACTOR.match(title)
 
 	parsed_title = title
+
 	if match:
 		parsed_title = match.group(1)
+
+	print([parsed_title, artists, tags, parodies, characters, pages, lang])
 
 	# Make the title lowercase
 	title = title.lower()
 
-	# See if this doujin has a magazine associated with it
-	# (girls forM is annoying because it doesn't have a COMIC, so I have to use another regex)
-	# (so is Weekly Kairakuten)
-	pattern1 = re.compile(r".*\(\s*comic\s*(.+?)\s*(?:vol\.)?\s*((\d|-|#|,|\s)*)\)")
-	pattern2 = re.compile(r".*\(\s*girls\s*form\s*(?:vol\.)?\s*(.+?)\)")
-	pattern3 = re.compile(r".*\(\s*weekly\s*kairakuten\s*(?:vol\.)?\s*(.+)\)")
-
 	magazine_name = None
 	magazine_issue = None
 
-	match1 = re.match(pattern1, title)
-	match2 = re.match(pattern2, title)
-	match3 = re.match(pattern3, title)
+	match1 = MAGAZINE_REGEX.match(title)
+	match2 = GIRLS_FORM_REGEX.match(title)
+	match3 = WEEKLY_MAG_REGEX.match(title)
 
 	# Extract the magazine issue and name
 	if match1 is not None:
@@ -235,25 +269,25 @@ async def check_link(link):
 	licensed = False
 
 	# If this is in a licensed magazine, check if it's in a licensed issue
-	if magazine_name in licensed_magazines:
-		try:
-			licensed = licensed_magazines[magazine_name][0](magazine_name, magazine_issue)
-		except Exception:
-			# Something has gone very wrong with a regular expression while trying to fetch the issue.
-			# In other words, the regular expression has failed. Ignore the post - the mods will remove it if it's
-			# actually licensed.
-			traceback.print_exc()
-			print("Error while detecting magazine. Setting licensed to false and continuing.")
+	if magazine_name and magazine_issue:
+		if magazine_name in LICENSED_MAGAZINES["always_licensed"]:
+			licensed = True
+		elif magazine_name in LICENSED_MAGAZINES["partially_licensed"]:
+			try:
+				licensed = date_num_compare(magazine_name, magazine_issue)
+			except Exception:
+				# Something has gone very wrong with a regular expression while trying to fetch the issue.
+				# In other words, the regular expression has failed. Ignore the post - the mods will remove it if it's
+				# actually licensed.
+				traceback.print_exc()
+				print("Error while detecting magazine. Setting licensed to false and continuing.")
 
-	market = "2d-market.com" in title
+		if licensed:
+			return magazine_name.upper() + " " + magazine_issue, market, [parsed_title, artists, tags, parodies, characters,
+																		pages, lang]
 
-	print([parsed_title, artists, tags, parodies, characters, pages, lang])
+	return None, market, [parsed_title, artists, tags, parodies, characters, pages, lang]
 
-	if licensed:
-		return magazine_name.upper() + " " + magazine_issue, market, [parsed_title, artists, tags, parodies, characters,
-		                                                              pages, lang]
-	else:
-		return None, market, [parsed_title, artists, tags, parodies, characters, pages, lang]
 
 # a, b, data = asyncio.run(check_link('https://nhentai.net/g/374491/'))
 # print(a, b, data)
